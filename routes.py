117079b59795aa3c21c6a61a69b82c8a08ed7a71
Diego Fontalvo <d.andres2002@hotmail.com>
2025-05-22 22:16:44 -0500

    feat: add api prefix from settings

--- routes.py
+++ routes.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter
 from modules.users.routes.user_routes import router as users_router
 from modules.trips.routes.trip_routes import router as trips_router
 from modules.tips.routes.tip_routes import router as tips_router
+from config import settings
 
-
-router = APIRouter()
+router = APIRouter(prefix=settings.API_PREFIX)
 
 router.include_router(users_router, prefix="/users", tags=["users"])
 router.include_router(trips_router, prefix="/trips", tags=["trips"])
