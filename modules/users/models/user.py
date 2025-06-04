73f65073758807b7c2e51799ec2e4fc1ba3b248c
Diego Fontalvo <d.andres2002@hotmail.com>
2025-06-04 18:13:37 -0500

    wip: add initial migrations config

--- modules/users/models/user.py
+++ modules/users/models/user.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.expression import text
+from typing import List
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship, Mapped
 from db.session import Base
 
 
@@ -8,10 +8,21 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
+    cognito_id = Column(String, nullable=False, unique=True)
+    is_active = Column(Boolean, default=False)
+    user_type_id = Column(Integer, ForeignKey("users_types.id", ondelete="cascade"), nullable=False)
+    user_type: Mapped['UserType'] = relationship(back_populates="users")
 
+class UserType(Base):
+    __tablename__ = "users_types"
 
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    users: Mapped[List["User"]] = relationship(back_populates="user_type_id")
 
 
 
